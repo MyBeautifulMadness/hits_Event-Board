149d54b0eba40a888459d41f8af84df9a57e6d6e
kerrigan <kostya.alimov.96@mail.ru>
2025-09-07 17:58:49 +0700

    Initial commit

---
+++ src/main/java/com/example/event_board/EventBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.event_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventBoardApplication.class, args);
+	}
+
+}
